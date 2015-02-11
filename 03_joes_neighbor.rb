4c716fe0eb6235e67fa5c2c6d7ddf1ca10a5a8d2
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-11 09:40:37 -0700

    Complete third exercise, find Joe's neighbor's last name

--- 03_joes_neighbor.rb
+++ 03_joes_neighbor.rb
@@ -3,3 +3,5 @@ require_relative 'people'
 # What is the last name of the person sitting next to joe?
 #
 # HINT: Start by finding who joe is sitting next to, then find that person in the hash.
+neighbor = PEOPLE["joe"][:sitting_next_to].to_s
+puts PEOPLE[neighbor][:last_name]
